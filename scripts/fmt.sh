c475c43f8f2602a00b24afec33545cd23f5ccbce
Andris Raugulis <moo@arthepsy.eu>
2018-11-15 12:07:22 +0000

    Add build scripts.

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 _cdir=$(cd $(dirname $0) && pwd)
 cd -- "${_cdir}/.."
-for i in . ./pkg/* ./internal/pkg/*; do
-	cd -- "${_cdir}/.."
-	cd -- "$i"
-	go fmt
-	go vet
-done
+env GO111MODULE=on go fmt ./...
+env GO111MODULE=on go vet ./...
